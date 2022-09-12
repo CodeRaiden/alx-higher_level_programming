d551b3f1e36ee2b94724252e0809ec000edd274b
CodeRaiden <hadoken10@yahoo.com>
2022-09-12 07:13:56 -0700

    fixed 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
-        print("{:d}\n".format(value))
-    except:
-        return False
-    else:
-        return True
+        print("{:d}".format(value))
+        return(True)
+    except (ValueError, TypeError):
+        return(False)
