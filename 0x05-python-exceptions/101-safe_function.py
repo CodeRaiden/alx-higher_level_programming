1cb7360e44d10584d0a2b3aa88266bd6d21f1dc8
CodeRaiden <hadoken10@yahoo.com>
2022-09-12 07:23:04 -0700

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-import traceback as tb
+from sys import stderr
+
 
 def safe_print_integer_err(value):
-    ret = False
     try:
         print("{:d}".format(value))
-        ret = True
-    except (ValueError, TypeError) as message:
-        tb.print_exception(Exception,
-                           Exception(message),
-                           None,
-                           limit=0,
-                           chain=False)
-    finally:
-        return ret
+    except (ValueError, TypeError) as te:
+        stderr.write("Exception: {}\n".format(te))
+        return (False)
+    return (True)
