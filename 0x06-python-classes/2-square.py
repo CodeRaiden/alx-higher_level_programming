b8d702d01f8e2875821f122f93b5f27cf90e0091
CodeRaiden <hadoken10@yahoo.com>
2022-09-13 12:40:16 -0700

    add 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         """
         size: size of the square
         """
-        if isdigit(type(size)) == False:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
