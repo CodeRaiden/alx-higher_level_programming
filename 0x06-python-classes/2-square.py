dcbf64f8322a12c3cef41af78ef975e758d0f38d
CodeRaiden <hadoken10@yahoo.com>
2022-09-13 12:22:15 -0700

    add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Square class that defines a square
+Private instance attribute: size
+Instantiation with optional size
+'''
+
+
+class Square:
+    '''
+    Square class with private instance attribute
+    '''
+
+    def __init__(self, size=0):
+        '''
+        size: is the size of the square
+        '''
+
+        try:
+            if isdigit(size) == False:
+                raise TypeError("size must be an integer")
+            elif isdigit(size) < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
