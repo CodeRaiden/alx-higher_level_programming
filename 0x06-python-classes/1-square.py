e3ee0806ae57b377c04f4bbe9e14c5adae5b1675
CodeRaiden <hadoken10@yahoo.com>
2022-09-13 11:43:09 -0700

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Square class that defines a square
+Private instance attribute: size
+Instantiation with size
+'''
+
+
+class Square:
+    '''
+    Square class with private instance attribute
+    '''
+
+    def __init__(self, size):
+        '''
+        size: is the size of the square
+        '''
+
+        self.__size = size
