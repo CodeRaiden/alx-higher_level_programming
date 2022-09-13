91c761bdc2e5b3ba4b1e261a0a2c2f006ebe2e8a
CodeRaiden <hadoken10@yahoo.com>
2022-09-13 14:01:05 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Square class definition
+"""
+
+
+class Square:
+    """
+    Square class with private instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """
+    getter wrapper to retrieve the size property
+    """
+
+    @property
+    def size(self):
+        return self.__size
+    """
+    setter wrapper to set the size property
+    """
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+
+    def area(self):
+        """
+        Returns area of the square instance
+        """
+        return self.__size ** 2
