00815bd855878e23ff2848c7896de1440528d947
CodeRaiden <hadoken10@yahoo.com>
2022-09-27 12:34:15 -0700

    add 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,13 +6,8 @@ that writes a string to a text file
 
 
 def write_file(filename="", text=""):
-    i = 0
     """
     function writes string to a text file
     """
     with open(filename, mode="w", encoding="utf-8") as myFile:
         myFile.write(text)
-        for word in text:
-            for char in word:
-                i += 1
-    return (i) 
