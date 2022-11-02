7780c676a1ea777b32e5471fedb2b26379f414c4
CodeRaiden <hadoken10@yahoo.com>
2022-11-01 20:38:13 -0700

    fix

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `second_table` (
 -- insert records into second_table
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
 (1, 'John', 10),
-(2, 'Mike', 3),
-(3, 'Tom', 14),
-(4, 'Sam', 8);
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
