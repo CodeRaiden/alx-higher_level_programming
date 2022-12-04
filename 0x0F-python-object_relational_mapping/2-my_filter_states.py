b38f04d61c1f70f9f907066b1b735aa821d0c949
CodeRaiden <hadoken10@yahoo.com>
2022-12-04 01:43:44 -0800

    add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument.
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY states.id in ASC".format(argv[4]))
+    states = cur.fetchall()
+    for row in states:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    conn.close()
