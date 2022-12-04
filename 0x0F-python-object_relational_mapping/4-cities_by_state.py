461d91443ebd3ba7d2829b2c5ed50a8adc110731
CodeRaiden <hadoken10@yahoo.com>
2022-12-04 02:41:30 -0800

    add 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the db
+takes 3 arguments
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    sts = cur.fetchall()
+    for row in sts:
+        print(row)
+    cur.close()
+    conn.close()
