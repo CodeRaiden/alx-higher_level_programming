6cfd304e8a297f0634f45969a9fe8e0cd61aef47
CodeRaiden <hadoken10@yahoo.com>
2022-12-03 22:06:19 -0800

    add 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,11 +8,11 @@ should take 3 arguments username, passwd and db name
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    curr = db.cursor()
-    curr.execute("SELECT * FROM states where name LIKE 'N%'\
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states where name LIKE 'N%'\
                  ORDER BY states.id ASC")
-    state = curr.fetchall()
+    state = cur.fetchall()
     for row in state:
         if row[1][0] == 'N':
             print(r)
