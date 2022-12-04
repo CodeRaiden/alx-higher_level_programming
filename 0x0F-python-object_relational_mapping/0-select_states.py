3ce9ebf9e604f02a9ec9765b5962d781ead5a830
CodeRaiden <hadoken10@yahoo.com>
2022-12-03 20:09:04 -0800

    add 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-script lists all state from db hbtn_0e_usa
-takes 3 args mysql username, mysql passwd, db name
+script lists all states from db hbtn_0e_0_usa
+takes 3 args mysql username, mysql password
+database name
 """
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(username=argv[1], password=argv[2], database=argv[3])
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
     state_li = cur.fetchall()
