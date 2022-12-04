855d9e718c177bc86441077583b3772bb4a4051b
CodeRaiden <hadoken10@yahoo.com>
2022-12-03 20:52:30 -0800

    added 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N
+takes 3 arguments username, passwd and db name
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    conn = MySQLdb.connect(name=argv[1], passwd=arg[2], db=argv[3])
+    cur = conn.cursor
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+            ORDER BY states.id ASC")
+    states = cur.fetchall()
+    for row in states:
+        if row[1][0] == 'N':
+            print(row)
+    conn.close()
+    cur.close()
