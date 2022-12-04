6a480939495b1d4fcd99aca28639423368827abb
CodeRaiden <hadoken10@yahoo.com>
2022-12-04 01:56:44 -0800

    add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,11 +11,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}'\
-            ORDER BY states.id in ASC".format(argv[4]))
+             ORDER BY states.id in ASC".format(argv[4]))
     sts = cur.fetchall()
     for row in sts:
         if row[1] == argv[4]:
             print(row)
     cur.close()
     conn.close()
-
