1f4c9fb428e7ebf45984a5bed05d15727985b0ae
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 13:00:00 -0800

    initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,16 @@
 '''
 Makes a request and prints information about the body of the response
 '''
-import urllib.request
 
+from urllib.request import urlopen
 
+URL = 'https://intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        body = resp.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
+if __name__ == '__main__':
+
+    with urlopen(URL) as r:
+        body = r.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode())
