cbb43fe929184f686966d38e6b2e87fa77d71715
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 13:19:18 -0800

    initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
+import urllib.request
+'''
+Makes a request and prints information about the body of the response
+'''
 
-from urllib.request import urlopen
-
-URL = 'https://intranet.hbtn.io/status'
-
-
-
-
-if __name__ == '__main__':
-
-    with urlopen(URL) as r:
-        body = r.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode())
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
