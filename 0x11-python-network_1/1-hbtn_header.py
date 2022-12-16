29961539fe12bb581dfe2b16a79fdc2e72f6479e
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 13:37:18 -0800

    fix 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 '''
-recieves, sends a request to the URL and displays the value of the X-Request-Id
+Makes a request to a URL and displays the value of the X-Request-Id header
 '''
-import urllib.request
+
 import sys
+from urllib.request import urlopen
 
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+    if len(sys.argv) != 2:
+        print('Usage: ', __file__, 'URL', file=sys.stderr)
+        sys.exit(2)
+
+    with urlopen(sys.argv[1]) as resp:
+        print(resp.getheader('X-Request-Id'))
