71cd44be14f9d2c5c762d833969f11ca206a2927
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 14:26:32 -0800

    add 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from urllib.error import URLError, HTTPError
-from sys import argv
-"""
-script that takes in url and sends a request to url
-displays body of response
-"""
+'''
+Sends a request to a URL and displays the body of the response
+'''
+
+import sys
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
 
 
-if __name__ == "__main__":
-    rep = urllib.request.Request(argv[1])
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print('Usage: ', __file__, 'URL', file=sys.stderr)
+        sys.exit(1)
+
     try:
-        with urllib.request.urlopen(rep) as reply:
-            print(reply.read().decode(encoding="utf-8"))
-    except URLError as err:
-        print("Error code: {}".format(err.code))
+        with urlopen(sys.argv[1]) as r:
+            print(r.read().decode())
+    except HTTPError as exc:
+        print('Error code:', exc.code)
+        sys.exit(1)
