a927ca380149780ac1e347dc1bd0064925b2927b
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 15:48:21 -0800

    add 4-hbtn_status.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that takes in a url to send a request to the url
+and display the values of the variable X-Request-Id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.header['X-Request-Id'])
