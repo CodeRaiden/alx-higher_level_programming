89075d878dc4c8aab28aa0a484ae82724e2d0b0f
CodeRaiden <hadoken10@yahoo.com>
2022-12-16 16:02:10 -0800

    fix 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-script that takes in a url to send a request to the url
-and display the values of the variable X-Request-Id
-"""
+'''
+Makes a request to a URL and displays the value of the X-Request-Id header
+'''
+
+import sys
 import requests
-from sys import argv
 
 
-if __name__ == "__main__":
-    r = requests.get(argv[1])
-    print(r.header.get('X-Request-Id'))
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print('Usage: ', __file__, 'URL', file=sys.stderr)
+        sys.exit(1)
+
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
